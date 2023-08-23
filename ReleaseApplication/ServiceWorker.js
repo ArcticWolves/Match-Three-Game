9a1cccbeddba2d0c40360024167b958864b288d2
ArcticWolves <roma.keivan@gmail.com>
2023-08-23 23:15:21 +0200

    first release App

---
+++ ReleaseApplication/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "ArcticWolves-MatchGems-0.0.6a";
+const contentToCache = [
+    "Build/Unity_cache.loader.js",
+    "Build/Unity_cache.framework.js",
+    "Build/Unity_cache.data",
+    "Build/Unity_cache.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
